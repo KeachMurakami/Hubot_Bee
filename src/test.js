d25e4713e38579a605eafaeff3d01cd59c9ea4ee
MurakamiKeach <m.keach.11@gmail.com>
2016-02-20 19:07:23 +0900

    Minor

--- src/test.js
+++ src/test.js
@@ -16,7 +16,11 @@
 // Description:
 //  respond Hi
 
-var myObj = new Object();
+
+module.exports = function(robot) {
+  robot.respond(/(.*)どこ？/i, function(msg) {
+
+  	var myObj = new Object();
 	myObj['XX'] = [420, 20160101];
 	myObj['あれ'] = [420, 20160101];
 	myObj['これ'] = [415, 20160202];
@@ -25,8 +29,6 @@ var myObj = new Object();
 	myObj['電源タップ'] = ['411窓側棚 (右下段)', 20160203];
 	myObj['実験系廃棄物処理依頼伝票'] = ['412の東側棚 (吉井さんの横の棚)', 20151212];
 
-module.exports = function(robot) {
-  robot.respond(/(.*)どこ？/i, function(msg) {
     msg.send(msg.match[1] + "は" + myObj[msg.match[1]][1] + "時点では" + myObj[msg.match[1]][0] + "にありました");
   });
 };
